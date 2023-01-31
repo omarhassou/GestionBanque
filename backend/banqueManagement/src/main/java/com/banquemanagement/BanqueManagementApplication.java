d694e0cf255cd67569c3f34373dfa63545c99bf6
omarhassou <omarhss11111234@gmail.com>
2023-01-31 18:40:56 +0100

    first commit

---
+++ backend/banqueManagement/src/main/java/com/banquemanagement/BanqueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.banquemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanqueManagementApplication.class, args);
+    }
+
+}
---
+++ backend/banqueManagement/src/test/java/com/banquemanagement/BanqueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
